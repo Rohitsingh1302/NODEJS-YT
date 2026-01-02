76821e9c14529188f2a217db88b6d0a6decd3418
rohit1302 <rohitnew1302@gmail.com>
2026-01-02 23:59:38 +0530

    editing methods.js

--- VID9/methods.js
+++ VID9/methods.js
@@ -1,1 +1,20 @@
 //get,post.put,patch,delete methods;
+//get method
+// app.get('/about',(req,res)=>{
+//     res.send("hello this is about page");
+//post method
+// app.post('/about',(req,res)=>{
+//     res.send("hello this is about page");
+//put method
+// app.put('/about',(req,res)=>{
+//     res.send("hello this is about page");
+//patch method
+// app.patch('/about',(req,res)=>{
+//     res.send("hello this is about page");
+//delete method
+// app.delete('/about',(req,res)=>{
+//     res.send("hello this is about page");
+//all methods
+app.all('/about',(req,res)=>{
+    res.send("hello this is about page");
+})
