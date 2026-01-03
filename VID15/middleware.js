32419cf807841dd9c1798aad5cfc6685a7f06143
rohit1302 <rohitnew1302@gmail.com>
2026-01-03 23:58:53 +0530

    done with another middleware

--- VID15/middleware.js
+++ VID15/middleware.js
@@ -5,9 +5,13 @@ app.use(exp.urlencoded({extended: false}));
 
 app.use((req,res,next)=>{
   console.log("hello from middleware");
-    next();
+  next();
 });
 
+app.use((req,res,next)=>{
+  console.log("hello from middleware 2");
+  next();
+});
 app.get("/",(req,res)=>{
     res.send("HELLO ROHIT FROM HOME PAGE");
 });
@@ -16,4 +20,6 @@ app.get("/users",(req,res)=>{
      res.json({message: "No users data available"});
 }); 
 
-app.listen(8000,console.log("SERVER RUNNING"));
+app.listen(8000, () => {
+    console.log("SERVER RUNNING");
+});
