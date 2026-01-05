76b2e7602be1a6164a1725c328e1b958d9b66a54
rohit1302 <rohitnew1302@gmail.com>
2026-01-05 23:23:14 +0530

    done with set cookies

--- VID25/cookies..js
+++ VID25/cookies..js
@@ -1,8 +1,23 @@
-const express = require("express");
+const express = require("express")();
 const cookieParser = require("cookie-parser");
-
-const app = express();
-const PORT = 3000;
 
 // middleware
 app.use(cookieParser());
+
+// Home route
+app.get("/", (req, res) => {
+  res.send("Welcome to Cookie Demo");
+});
+
+// SET COOKIE
+app.get("/set-cookie", (req, res) => {
+  res.cookie("username", "Rohit", {
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
+    httpOnly: true
+  });
+  res.send("Cookie has been set");
+});
+
+app.listen(8000, () => {
+  console.log("Server running" );
+});
