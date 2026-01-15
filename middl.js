dcd351251c951723ab2522c86305ee0075dc4711
rohit1302 <rohitnew1302@gmail.com>
2026-01-15 23:25:35 +0530

    middleware done

---
+++ middl.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+// Middleware
+app.use((req, res, next) => {
+  console.log("Middleware executed");
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
